fca97aa45c198cca1bad1fe71cca0ef932f948f9
Sparsh Srivastava <spars16@vt.edu>
2018-09-22 17:09:43 -0400

    Initial Commit

---
+++ stroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stroll
+//
+//  Created by Sparsh Srivastava on 9/22/18.
+//  Copyright © 2018 Sparsh Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
